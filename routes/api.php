d0d41a326f0c08cb4ba9bf90ff94077e6b4685a1
Aurnab <zubayeraurnab@gmail.com>
2024-11-16 22:06:03 +0600

    make the kernel.php and middleware files

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
@@ -10,3 +11,13 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('/categories', CategoryController::class);
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/post', PostController::class);
+
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+    Route::delete('logout', [AuthController::class, 'logout']);
+});
